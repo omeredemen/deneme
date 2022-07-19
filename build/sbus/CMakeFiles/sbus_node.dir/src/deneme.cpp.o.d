ae021eb51a85b4028457f715847ec9735e43a917
omeredemen <edemeno2525@gmail.com>
2022-07-19 14:39:06 +0300

    Latest commit

--- build/sbus/CMakeFiles/sbus_node.dir/src/deneme.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sbus_node.dir/src/deneme.cpp.o: \
- /home/omer/Desktop/deneme_ws/src/sbus/src/deneme.cpp \
- /usr/include/stdc-predef.h
--- build/sbus/CMakeFiles/sbus_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sbus_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sbus_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
